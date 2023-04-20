df85fbca3a170ea0d81103c0ee3b2082e107e790
JHANANI27 <108513771+JHANANI27@users.noreply.github.com>
2023-04-20 19:46:47 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,167 @@
+const logotext = "JHANANI ";
+const meta = {
+    title: "Jhanani Shri ",
+    description: "I’m Jhanani- Engineer _ web devloper,currently studying",
+};
+
+const introdata = {
+    title: "I’m Jhanani",
+    animated: {
+        first: "Cooooool",
+        second: "I do codeee",
+        third: "Love coding!!!",
+    },
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    your_img_url: "https://i.pinimg.com/736x/fd/af/02/fdaf02c180313760412dee70fb8d72a0.jpg"  ,
+};
+
+const dataabout = {
+    title: "A bit about my self",
+    aboutme: "I am currently persuing Engineering At Sri Krishna Instituitions.I have been learning coding for about 2 years.I have basic knowledge on HTML and CSS.I am pretty good with DBMS and C++",
+};
+const worktimeline = [{
+        jobtitle: "Ametuer web developer",
+        where: "Seedlabs",
+        date: "2020",
+        
+    },
+    {
+        jobtitle: "Ameteur game app developer",
+        where: "SeedLabs",
+        date: "2019",
+    },
+    {
+        jobtitle: "Ameteur front end developer",
+        where: "Seedlabs",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 75,
+    },
+    {
+        name: "DBMS",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "C++",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "UI/UX design aims to create a positive user experience that encourages customers to stick with a brand or product. While a UX Designer decides how the user interface works, the UI designer decides how the user interface looks.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Mobile application development is the process to making software for smartphones and digital assistants, most commonly for Android and iOS. The software can be preinstalled on the device, downloaded from a mobile app store or accessed through a mobile web browser. The programming and markup languages used for this kind of software development include Java, Swift, C# and HTML5.",
+    },
+    {
+        title: "Website Developement",
+        description: "Web development, also known as website development, refers to the tasks associated with creating, building, and maintaining websites and web applications that run online on a browser. It may, however, also include web design, web programming, and database management.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://images.pexels.com/photos/963436/pexels-photo-963436.jpeg?cs=srgb&dl=pexels-megan-forbes-963436.jpg&fm=jpg",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1622616332975-3ea56ed51e46?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NXx8fGVufDB8fHx8&w=1000&q=80",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://wallpapers.com/images/featured/pink-iphone-p4b7uisuxphx3z7s.jpg",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREUzVi4Rel-0ewugTVqwO97bLZQZAwGtSEGA&usqp=CAU",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://media.istockphoto.com/id/958522470/photo/group-of-orange-slice-in-pink-color-fruit-and-summer-concept.jpg?b=1&s=170667a&w=0&k=20&c=uO62EiJkLlSrYoy3bKCbouBGZxk2uLUtFcCbDOEBi1E=",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://static.vecteezy.com/system/resources/previews/003/107/021/large_2x/abstract-background-of-lights-glowing-pink-with-colorful-strokes-photo.jpg",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+   /* {
+        img: "https://images.pling.com/img/00/00/64/96/96/1821084/anime-night-neon-191.jpg",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+   /* {
+        img: "https://wallpapers.com/images/featured/z7gzw2yxujj6t305.jpg",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    /*{
+        img: "https://wallpapers.com/images/file/neon-pink-aesthetic-flamingo-light-b7da11cof3cr9mgv.jpg",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    /*{
+        img: "https://i.pinimg.com/736x/92/a9/ed/92a9ed84e0e3a8aea07224a32ec19a88.jpg",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+   /* {
+        img: "https://e1.pxfuel.com/desktop-wallpaper/813/22/desktop-wallpaper-glowing-city-in-night-blue-and-pink-city-thumbnail.jpg",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    /*{
+        img: "https://e0.pxfuel.com/wallpapers/186/705/desktop-wallpaper-neon-sign-aesthetic-pink-neon-lights.jpg",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },*/
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "jhanani2703@gmail.com",
+    YOUR_PHONE: "8870270333",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
